d8288ac2cb124970713db9e3edd4e1820afeceed
Horleus <Horleus@192.168.0.102>
2020-05-15 23:07:01 +0300

    If ctg

--- src/Lab10.cpp
+++ src/Lab10.cpp
@@ -8,8 +8,23 @@ int main()
 
 	setlocale(LC_ALL, "Ukrainian");
 
+    cout << "x";
+    cin >> x;
+    cout << endl;
 
+    cout << "y";
+    cin >> y;
+    cout << endl;
 
+    if((x<0) && (y<=0))
+    {
+    v=x*y;
+    }
+
+    if ((0<=x<10) && (0<y<=9));
+    {
+    v=ctg*x/y;
+    }
 
     return 0;
 }
