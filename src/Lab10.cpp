86c81f599b53f17a8948e534797da1c4f4a01995
Horleus <Horleus@192.168.0.102>
2020-05-15 20:15:16 +0300

    entry

--- src/Lab10.cpp
+++ src/Lab10.cpp
@@ -1,15 +1,16 @@
-//============================================================================
-// Name        : Lab10.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
 #include <iostream>
+#include <math.h>
 using namespace std;
 
-int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	return 0;
+int main()
+{
+	double v,x,y,ctg;
+
+	setlocale(LC_ALL, "Ukrainian");
+
+
+
+
+    return 0;
 }
+
